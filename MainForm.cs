1386e8d423f9aa8fad7f6714f1f48063c579f05a
LAPTOP-3N644IDG\Tuong <tuongporo9x2004@gmail.com>
2024-03-29 14:16:26 +0700

    Revert "no message"
    This reverts commit 1995cc228a9cdc65bb6a4d2100eea2e7dd4363ee.

--- MainForm.cs
+++ MainForm.cs
@@ -8,24 +8,5 @@ namespace MusicPlayer
         {
             InitializeComponent();
         }
-
-        private void btnExitMainForm_Click(object sender, System.EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void btnDangNhap_Click(object sender, System.EventArgs e)
-        {
-            FormLogin formLogin = new FormLogin();
-            formLogin.Show();
-            this.Hide();
-        }
-
-        private void btnDangKy_Click(object sender, System.EventArgs e)
-        {
-            FormRegister formRegister = new FormRegister();
-            formRegister.Show();
-            this.Hide();
-        }
     }
 }
