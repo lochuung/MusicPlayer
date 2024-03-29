1995cc228a9cdc65bb6a4d2100eea2e7dd4363ee
LAPTOP-3N644IDG\Tuong <tuongporo9x2004@gmail.com>
2024-03-29 14:15:52 +0700

    no message

---
+++ FormLogin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicPlayer
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExitMainForm_Click(object sender, EventArgs e)
+        {
+            MainForm mainForm = new MainForm();
+            mainForm.Show();
+        }
+    }
+}
