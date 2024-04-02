3269058e604a18ba2c1e50da4f07fa5889052566
LAPTOP-3N644IDG\Tuong <tuongporo9x2004@gmail.com>
2024-04-02 09:43:47 +0700

    test database

---
+++ Database/DBConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+namespace MusicPlayer.Database
+{
+    class DBConnection
+    {
+        SqlConnection connection = new SqlConnection(Properties.Settings.Default.connStr);
+        SqlCommand command;
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable table = new DataTable();
+        String currentUser = "";
+    }
+}
