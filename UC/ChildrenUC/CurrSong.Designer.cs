e7c7288dfb6b3095cf3e927e86219504160e227a
LAPTOP-3N644IDG\Tuong <tuongporo9x2004@gmail.com>
2024-04-26 15:45:24 +0700

    UI CurrentSong

--- UC/ChildrenUC/CurrSong.Designer.cs
+++ UC/ChildrenUC/CurrSong.Designer.cs
@@ -33,17 +33,22 @@ namespace MusicPlayer.UC.ChildrenUC
             this.lblTenBaiHat = new System.Windows.Forms.Label();
             this.lblTenCaSi = new System.Windows.Forms.Label();
             this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
-            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.ptrImage)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // ptrImage
             // 
             this.ptrImage.Image = global::MusicPlayer.Properties.Resources.music_1001px;
             this.ptrImage.ImageRotate = 0F;
-            this.ptrImage.Location = new System.Drawing.Point(24, 141);
+            this.ptrImage.Location = new System.Drawing.Point(46, 159);
             this.ptrImage.Name = "ptrImage";
-            this.ptrImage.Size = new System.Drawing.Size(412, 371);
+            this.ptrImage.Size = new System.Drawing.Size(388, 354);
             this.ptrImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.ptrImage.TabIndex = 0;
             this.ptrImage.TabStop = false;
@@ -52,7 +57,7 @@ namespace MusicPlayer.UC.ChildrenUC
             // 
             this.lblTenBaiHat.AutoSize = true;
             this.lblTenBaiHat.Font = new System.Drawing.Font("Segoe UI Black", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTenBaiHat.Location = new System.Drawing.Point(90, 540);
+            this.lblTenBaiHat.Location = new System.Drawing.Point(91, 532);
             this.lblTenBaiHat.Name = "lblTenBaiHat";
             this.lblTenBaiHat.Size = new System.Drawing.Size(275, 38);
             this.lblTenBaiHat.TabIndex = 1;
@@ -63,7 +68,7 @@ namespace MusicPlayer.UC.ChildrenUC
             // 
             this.lblTenCaSi.AutoSize = true;
             this.lblTenCaSi.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTenCaSi.Location = new System.Drawing.Point(128, 601);
+            this.lblTenCaSi.Location = new System.Drawing.Point(131, 594);
             this.lblTenCaSi.Name = "lblTenCaSi";
             this.lblTenCaSi.Size = new System.Drawing.Size(181, 23);
             this.lblTenCaSi.TabIndex = 2;
@@ -82,34 +87,85 @@ namespace MusicPlayer.UC.ChildrenUC
             this.guna2Button1.ForeColor = System.Drawing.Color.Black;
             this.guna2Button1.HoverState.BorderColor = System.Drawing.Color.Black;
             this.guna2Button1.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.guna2Button1.Location = new System.Drawing.Point(626, 79);
+            this.guna2Button1.Location = new System.Drawing.Point(722, 91);
             this.guna2Button1.Name = "guna2Button1";
             this.guna2Button1.Size = new System.Drawing.Size(195, 45);
             this.guna2Button1.TabIndex = 4;
             this.guna2Button1.Text = "Lời bài hát ";
             // 
-            // flowLayoutPanel2
+            // label1
             // 
-            this.flowLayoutPanel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(506, 141);
-            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(459, 500);
-            this.flowLayoutPanel2.TabIndex = 0;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label1.Location = new System.Drawing.Point(51, 636);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 23);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Album : ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label2.Location = new System.Drawing.Point(65, 594);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ca sĩ : ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(182, 636);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = ".......";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.richTextBox1);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(506, 156);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(633, 594);
+            this.flowLayoutPanel1.TabIndex = 8;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Lavender;
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(630, 594);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
             // 
             // CurrSong
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.guna2Button1);
             this.Controls.Add(this.lblTenCaSi);
             this.Controls.Add(this.lblTenBaiHat);
             this.Controls.Add(this.ptrImage);
             this.Name = "CurrSong";
-            this.Size = new System.Drawing.Size(1038, 753);
+            this.Size = new System.Drawing.Size(1156, 753);
             this.Load += new System.EventHandler(this.CurrSong_Load);
             ((System.ComponentModel.ISupportInitialize)(this.ptrImage)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,6 +177,10 @@ namespace MusicPlayer.UC.ChildrenUC
         private System.Windows.Forms.Label lblTenBaiHat;
         private System.Windows.Forms.Label lblTenCaSi;
         private Guna.UI2.WinForms.Guna2Button guna2Button1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
