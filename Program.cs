c5a9559d718096386b7c95d59cf9e9a7d33a518b
LAPTOP-3N644IDG\Tuong <tuongporo9x2004@gmail.com>
2024-04-27 12:59:17 +0700

    ForgetPW Form

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace MusicPlayer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormRegister());
+            Application.Run(new View.SecurityCode());
 
             // ZingMp3Api api = new ZingMp3Api();
             // var result = ZingMp3ApiUtils.GetSongInfo(api, "Z7Z7A7F0");
